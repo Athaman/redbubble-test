fd47bbf5ef63274e1b0caa259134a416a2455010
Redbubble Applicant <me@example.com>
2021-09-12 19:48:03 +1000

    added a high level pseudocode of that I think I'm doing

---
+++ index.js
@@ -0,0 +1,9 @@
+// Accept two arguments from the command line, first is a cart, the second a list of base prices.
+
+// Read the files from the local drives and save them as objects cart and prices
+
+//  Set up a total to track the cost
+
+//  iterate over the cart object and look up each item in the price object then add to the total
+
+//  return final cost to the user
