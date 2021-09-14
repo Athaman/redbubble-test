47f19c469a8e1dad3db89c09b35e6f1971aa3cba
Redbubble Applicant <me@example.com>
2021-09-14 17:43:18 +1000

    pulled the main calculation function out to its own file to decouple from the entry file

---
+++ calculateCart.js
@@ -0,0 +1,28 @@
+const readJSONToObject = require('./helpers/readJSONToObject');
+const getTypePrices = require('./getTypePrices');
+const findPriceByOptions = require('./findPriceByOptions');
+
+const calculateCart = async (cartFileLocation, pricesFileLocation) => {
+  // console.log(cartFileLocation, pricesFileLocation);
+  // Read the files from location given back as objects. Currently sequential, could be made parallel if files are large.
+  const cart = await readJSONToObject(cartFileLocation);
+  const prices = await readJSONToObject(pricesFileLocation);
+
+  //  Set up a total to track the cost
+  let total = 0;
+
+  //  iterate over the cart object and look up each item in the price object then add to the total
+  for (itemKey in cart) {
+    const item = cart[itemKey];
+    // Get the items from the price list that correspond to the item type of this item.
+    const itemPrices = getTypePrices(item, prices);
+    // Find the matching options version and add its base price to the total
+    itemPrices.forEach((itemPrice) => {
+      total += findPriceByOptions(item, itemPrice);
+    });
+  }
+  //  return final cost to the user
+  console.log(total);
+  return total;
+};
+module.exports = calculateCart;
--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-const readJSONToObject = require('./helpers/readJSONToObject');
-const getTypePrices = require('./getTypePrices');
-const findPriceByOptions = require('./findPriceByOptions');
+const calculateCart = require('./calculateCart');
 
-const main = async () => {
+const main = () => {
   // Accept two arguments from the command line, first is a cart, the second a list of base prices.
   const [, , cartFileLocation, pricesFileLocation] = [...process.argv];
-
-  // Read the files from location given back as objects. Currently sequential, could be made parallel if files are large.
-  const cart = await readJSONToObject(cartFileLocation);
-  const prices = await readJSONToObject(pricesFileLocation);
-
-  //  Set up a total to track the cost
-  let total = 0;
-
-  //  iterate over the cart object and look up each item in the price object then add to the total
-  for (itemKey in cart) {
-    const item = cart[itemKey];
-    // Get the items from the price list that correspond to the item type of this item.
-    const itemPrices = getTypePrices(item, prices);
-    // Find the matching options version and add its base price to the total
-    itemPrices.forEach((itemPrice) => {
-      total += findPriceByOptions(item, itemPrice);
-    });
-  }
-  //  return final cost to the user
-  console.log(total);
-  return `${total}\n`;
+  const cartCost = calculateCart(cartFileLocation, pricesFileLocation);
+  return `${cartCost}\n`;
 };
 
 main();
